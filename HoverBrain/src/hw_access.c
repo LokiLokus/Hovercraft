576a72b4d3c5cbca2bd2a6b9d2ea1ef088a0360a
Jakob Löw <jakob@m4gnus.de>
2017-09-25 09:16:58 +0200

    let the user define the port to use but auto determine if its a hardware PWM

--- HoverBrain/src/hw_access.c
+++ HoverBrain/src/hw_access.c
@@ -24,81 +24,44 @@ int hover_hw_controller_fin(hover_hw_controller* hwc)
 	gpioTerminate();
 }
 
-bool hover_get_gpiop(hover_hw_controller* hwc, u8 port)
-{
-	return (hwc->used_ports >> port) & 0x1 == 1;
-}
-void hover_set_gpiop(hover_hw_controller* hwc, u8 port, bool val)
-{
-	if(val){
-		hwc->used_ports |= 1 << port;
-	}
-	else{
-		hwc->used_ports &= ~(1 << port);
-	}
-}
-
-u8 hover_request_gpiop( hover_hw_controller* hwc, bool hwa)
-{
-	//for now I will assume that we have a pi 3
-	//if we ever need support for anything else we should do some port table
-	//of some kind, but I'm too lazy to do that right now
-	u8 port;
-	if(hwa){
-		port = 13;
-		if(hover_get_gpiop(hwc, port)){
-			port = 12;
-			if(hover_get_gpiop(hwc, port)){
-				return HOVER_INVALID_GPIOP;
-			}
-		}
-	}
-	else{
-		port = 2;
-		while(port < 28){
-			if(port == 12)port = 14; //12 and 13 are hwa pwm
-			else if(port == 18)port = 120; //18 and 19 are hwa pwm
-			if(hover_get_gpiop(hwc, port) == false)break;
-		}
-		if(port == 28)return HOVER_INVALID_GPIOP;
-	}
-	hover_set_gpiop(hwc, port, true);
-	return port;
-}
-
 int hover_hwo_init(hover_hwo* hwo, hover_hw_controller* hwc,
-				   bool hardware_assisted, hover_hwo_type hwot)
+				   hover_hwo_type hwot, u8 port)
 {
 	hwo->hwc = hwc;
-	hwo->pwm_hwa = hardware_assisted;
 	hwo->type = hwot;
-	//we need to find out what kind of frequencies and duty cycles are required
-	//for now we use some reasonable guessed values
+	hwo->gpiop = port;
+
 	switch (hwot){
-		case HOVER_HWO_TYPE_SERVO:{
-			hwo->pwm_freq = HOVER_HWO_SERVO_FREQ;
-			hwo->pwm_min_duty_cycle = HOVER_HWO_SERVO_CYCLE_MIN;
-			hwo->pwm_max_duty_cycle = HOVER_HWO_SERVO_CYCLE_MAX;
-		}break;
-		case HOVER_HWO_TYPE_MOTOR:{
-			hwo->pwm_freq = HOVER_HWO_MOTOR_FREQ;
-			hwo->pwm_min_duty_cycle = HOVER_HWO_MOTOR_CYCLE_MIN;
-			hwo->pwm_max_duty_cycle = HOVER_HWO_MOTOR_CYCLE_MAX;
-		}break;
+		case HOVER_HWO_TYPE_SERVO:
+			hwo->pwm_freq = HOVER_SERVO_FREQ;
+			hwo->pwm_min_duty_cycle = HOVER_SERVO_CYCLE_MIN;
+			hwo->pwm_max_duty_cycle = HOVER_SERVO_CYCLE_MAX;
+			break;
+		case HOVER_HWO_TYPE_MOTOR:
+			hwo->pwm_freq = HOVER_MOTOR_FREQ;
+			hwo->pwm_min_duty_cycle = HOVER_MOTOR_CYCLE_MIN;
+			hwo->pwm_max_duty_cycle = HOVER_MOTOR_CYCLE_MAX;
+			break;
 	}
-	hwo->gpiop = hover_request_gpiop(hwc, hwo->pwm_hwa);
-	if(hwo->gpiop == HOVER_INVALID_GPIOP)return HOVER_INVALID_GPIOP;
-	if(hwo->pwm_hwa){
-		gpioHardwarePWM(hwo->gpiop, hwo->pwm_freq, 0);
-	}
-	else{
-		//see http://abyz.co.uk/rpi/pigpio/cif.html#gpioServo
-		gpioPWM(hwo->gpiop, 0);
-		gpioSetPWMfrequency(hwo->gpiop, hwo->pwm_freq);
-		gpioSetPWMrange(hwo->gpiop, 1000000 / hwo->pwm_freq);
+
+	switch (port) {
+		//list of hardware PWM's
+		case 12:
+		case 13:
+		case 18:
+		case 19:
+			hwo->pwm_hwa = true;
+			gpioHardwarePWM(hwo->gpiop, hwo->pwm_freq, 0);
+			break;
+		default:
+			hwo->pwm_hwa = false;
+			//see http://abyz.co.uk/rpi/pigpio/cif.html#gpioServo
+			gpioPWM(hwo->gpiop, 0);
+			gpioSetPWMfrequency(hwo->gpiop, hwo->pwm_freq);
+			gpioSetPWMrange(hwo->gpiop, 1000000 / hwo->pwm_freq);
+			break;
 	}
 }
-
 int hover_hwo_fin(hover_hwo* hwo)
 {
 	int ec = 0;
@@ -108,9 +71,10 @@ int hover_hwo_fin(hover_hwo* hwo)
 	else{
 		ec = gpioHardwarePWM(hwo->gpiop, 0, 0);
 	}
-	hover_set_gpiop(hwo->hwc,hwo->gpiop,false);
+	hwo->hwc->used_ports &= ~(1 << hwo->gpiop);
 	return ec;
 }
+
 int hover_hwo_set_duty_fraction(hover_hwo* hwo, float fraction)
 {
 	u32 duty = hwo->pwm_min_duty_cycle;
