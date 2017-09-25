dbaf41463b71d41155fc8571741939d358337b2a
Jakob Löw <jakob@m4gnus.de>
2017-09-25 08:37:27 +0200

    make the hardware code not use different code style inside and make it use some constant defines for frequencies and duty cycles

--- HoverBrain/include/hw_access.h
+++ HoverBrain/include/hw_access.h
@@ -1,6 +1,14 @@
 #pragma once
 #include "types.h"
 
+#define HOVER_HWO_SERVO_FREQ 50
+#define HOVER_HWO_SERVO_CYCLE_MIN 1000
+#define HOVER_HWO_SERVO_CYCLE_MAX 2000
+
+#define HOVER_HWO_MOTOR_FREQ 50
+#define HOVER_HWO_MOTOR_CYCLE_MIN 0
+#define HOVER_HWO_MOTOR_CYCLE_MAX 2500
+
 typedef enum {
 	HOVER_HWO_TYPE_SERVO,
 	HOVER_HWO_TYPE_MOTOR,
@@ -20,13 +28,13 @@ typedef struct {
 	hover_hw_controller* hwc;
 	hover_hwo_type type;
 	//the gpio port
-	u8 gpiop;	
+	u8 gpiop;
 	//specifys whether or not this gpio port supports hardware assisted pwm
-	bool pwm_hwa; 
+	bool pwm_hwa;
 	ureg pwm_freq;	//frequency of pwm signal in hertz
 	ureg pwm_duty_cycle;	//duty cycle of pwm signal in microseconds
-	ureg pwm_min_duty_cycle; 
-	ureg pwm_max_duty_cycle; 
+	ureg pwm_min_duty_cycle;
+	ureg pwm_max_duty_cycle;
 } hover_hwo;
 
 int hover_hwo_init(hover_hwo* hwo, hover_hw_controller* hwc,
--- HoverBrain/src/hw_access.c
+++ HoverBrain/src/hw_access.c
@@ -1,7 +1,6 @@
 #include "../include/hw_access.h"
 #include "../include/hw_access.h"
 #include <pigpio.h>
-#include <stdio.h>
 
 #define HOVER_INVALID_GPIOP 255
 
@@ -19,16 +18,18 @@ int hover_hw_controller_fin(hover_hw_controller* hwc)
 {
 	for(int i = 0; i < 8 * sizeof(hwc->used_ports); i++){
 		if(hwc->used_ports>>i == 1){
-			
+
 		}
 	}
 	gpioTerminate();
 }
 
-bool hover_get_gpiop(hover_hw_controller* hwc, u8 port){
+bool hover_get_gpiop(hover_hw_controller* hwc, u8 port)
+{
 	return (hwc->used_ports >> port) & 0x1 == 1;
 }
-void hover_set_gpiop(hover_hw_controller* hwc, u8 port, bool val){
+void hover_set_gpiop(hover_hw_controller* hwc, u8 port, bool val)
+{
 	if(val){
 		hwc->used_ports |= 1 << port;
 	}
@@ -37,7 +38,8 @@ void hover_set_gpiop(hover_hw_controller* hwc, u8 port, bool val){
 	}
 }
 
-u8 hover_request_gpiop( hover_hw_controller* hwc, bool hwa){
+u8 hover_request_gpiop( hover_hw_controller* hwc, bool hwa)
+{
 	//for now I will assume that we have a pi 3
 	//if we ever need support for anything else we should do some port table
 	//of some kind, but I'm too lazy to do that right now
@@ -74,14 +76,14 @@ int hover_hwo_init(hover_hwo* hwo, hover_hw_controller* hwc,
 	//for now we use some reasonable guessed values
 	switch (hwot){
 		case HOVER_HWO_TYPE_SERVO:{
-			hwo->pwm_freq = 50;
-			hwo->pwm_min_duty_cycle = 1000;
-			hwo->pwm_max_duty_cycle = 2000;
+			hwo->pwm_freq = HOVER_HWO_SERVO_FREQ;
+			hwo->pwm_min_duty_cycle = HOVER_HWO_SERVO_CYCLE_MIN;
+			hwo->pwm_max_duty_cycle = HOVER_HWO_SERVO_CYCLE_MAX;
 		}break;
 		case HOVER_HWO_TYPE_MOTOR:{
-			hwo->pwm_freq = 50;
-			hwo->pwm_min_duty_cycle = 0;
-			hwo->pwm_max_duty_cycle = 2500;
+			hwo->pwm_freq = HOVER_HWO_MOTOR_FREQ;
+			hwo->pwm_min_duty_cycle = HOVER_HWO_MOTOR_CYCLE_MIN;
+			hwo->pwm_max_duty_cycle = HOVER_HWO_MOTOR_CYCLE_MAX;
 		}break;
 	}
 	hwo->gpiop = hover_request_gpiop(hwc, hwo->pwm_hwa);
@@ -97,7 +99,8 @@ int hover_hwo_init(hover_hwo* hwo, hover_hw_controller* hwc,
 	}
 }
 
-int hover_hwo_fin(hover_hwo *hwo){
+int hover_hwo_fin(hover_hwo* hwo)
+{
 	int ec = 0;
 	if(hwo->pwm_hwa == false){
 		ec = gpioPWM(hwo->gpiop, 0);
@@ -108,13 +111,12 @@ int hover_hwo_fin(hover_hwo *hwo){
 	hover_set_gpiop(hwo->hwc,hwo->gpiop,false);
 	return ec;
 }
-int hover_hwo_set_duty_fraction(hover_hwo* hwo, float fraction){
-	//it's theoretically cheaper to compute this if we store the diff instead
-	//but for now I don't care
-	u32 duty = hwo->pwm_min_duty_cycle +
-			   (hwo->pwm_max_duty_cycle - hwo->pwm_min_duty_cycle) * fraction;
+int hover_hwo_set_duty_fraction(hover_hwo* hwo, float fraction)
+{
+	u32 duty = hwo->pwm_min_duty_cycle;
+	duty += (hwo->pwm_max_duty_cycle - hwo->pwm_min_duty_cycle) * fraction;
 	duty *= hwo->pwm_freq;
-	printf("duty = %lu\n", duty);
+
 	if(hwo->pwm_hwa){
 		gpioHardwarePWM(hwo->gpiop, hwo->pwm_freq, duty);
 	}
