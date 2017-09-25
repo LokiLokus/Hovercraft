9b66838e12375cc861279c3963b6701e05d0379b
Jakob Löw <jakob@m4gnus.de>
2017-09-25 09:59:49 +0200

    hook the hardware controls to the internal event system, allowing to control the motors/servors from the webpanel

--- HoverBrain/src/hw_access.c
+++ HoverBrain/src/hw_access.c
@@ -1,10 +1,53 @@
-#include "../include/hw_access.h"
-#include "../include/hw_access.h"
+#include "../include/types.h"
+#include "../include/events.h"
+
+#include <stdlib.h>
 #include <pigpio.h>
 
-#define HOVER_INVALID_GPIOP 255
 
-int hover_hw_controller_init(hover_hw_controller* hwc)
+//TODO are these values correct?
+#define HOVER_SERVO_FREQ 50
+#define HOVER_SERVO_CYCLE_MIN 1000
+#define HOVER_SERVO_CYCLE_MAX 2000
+
+//TODO are these values correct?
+#define HOVER_MOTOR_FREQ 50
+#define HOVER_MOTOR_CYCLE_MIN 0
+#define HOVER_MOTOR_CYCLE_MAX 2500
+
+typedef enum {
+	HOVER_HWO_TYPE_SERVO,
+	HOVER_HWO_TYPE_MOTOR,
+} hover_hwo_type;
+
+//hardware controller
+typedef struct {
+	//apparently there is no pigpio instance and it uses some weird static bs
+	u32 used_ports;	//bitfield of used ports
+} hover_hw_controller;
+
+//hardware object
+typedef struct {
+	hover_hw_controller* hwc;
+	hover_hwo_type type;
+	//the gpio port
+	u8 gpiop;
+	//specifys whether or not this gpio port supports hardware assisted pwm
+	bool pwm_hwa;
+	ureg pwm_freq;	//frequency of pwm signal in hertz
+	ureg pwm_duty_cycle;	//duty cycle of pwm signal in microseconds
+	ureg pwm_min_duty_cycle;
+	ureg pwm_max_duty_cycle;
+} hover_hwo;
+
+typedef struct {
+	hover_hw_controller hwc;
+	hover_hwo hover;
+	hover_hwo forward;
+	hover_hwo steering;
+} hover_hw_context;
+
+int hover_hwc_init(hover_hw_controller* hwc)
 {
 	gpioTerminate();
 
@@ -13,8 +56,7 @@ int hover_hw_controller_init(hover_hw_controller* hwc)
 	ec = gpioInitialise();
 	if(ec < 0)return ec;
 }
-
-int hover_hw_controller_fin(hover_hw_controller* hwc)
+int hover_hwc_fin(hover_hw_controller* hwc)
 {
 	for(int i = 0; i < 8 * sizeof(hwc->used_ports); i++){
 		if(hwc->used_ports>>i == 1){
@@ -87,5 +129,29 @@ int hover_hwo_set_duty_fraction(hover_hwo* hwo, float fraction)
 	else{
 		gpioPWM(hwo->gpiop, duty);
 	}
+}
 
+void hover_hardware_event_handler(const hover_event_t *event, void *data)
+{
+	if(event->argLength != 4)
+		return;
+
+	hover_hwo *hwo = data;
+	s32 value = hover_event_argument_as(event, s32);
+
+	//TODO let the frontend send the duty cycle? or the fraction?
+	hover_hwo_set_duty_fraction(hwo, (float)value / 1000);
+}
+void hover_hardware_init(u8 steering, u8 hover, u8 forward)
+{
+	hover_hw_context *ctx = malloc(sizeof(hover_hw_context));
+	hover_hwc_init(&ctx->hwc);
+
+	hover_hwo_init(&ctx->steering, &ctx->hwc, HOVER_HWO_TYPE_SERVO, steering);
+	hover_hwo_init(&ctx->hover, &ctx->hwc, HOVER_HWO_TYPE_MOTOR, hover);
+	hover_hwo_init(&ctx->forward, &ctx->hwc, HOVER_HWO_TYPE_MOTOR, forward);
+
+	hover_event_add_handler(HOVER_EVENT_STEERING, hover_hardware_event_handler, &ctx->steering);
+	hover_event_add_handler(HOVER_EVENT_HOVER, hover_hardware_event_handler, &ctx->hover);
+	hover_event_add_handler(HOVER_EVENT_FORWARD, hover_hardware_event_handler, &ctx->forward);
 }
