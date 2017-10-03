538afed79618336375bb6dd3170d28dd7d8476e7
Jakob Löw <jakob@m4gnus.de>
2017-10-03 13:09:16 +0200

    add a small web front end for generating events for testing (no sending yet)

---
+++ HoverServer/WebContent/Control.js
@@ -0,0 +1,68 @@
+var eventId = document.getElementById("eventId");
+var eventLittleEndian = document.getElementById("littleEndian");
+var eventArgs = document.getElementById("eventArgs");
+
+var events = [
+	//control related events
+	"hover", //sets the speed of the hover motor
+	"forward", //sets the speed of the manouvering motor
+	"steering", //sets the steering
+	"stopall", //stops all motors
+	"turbo", //enable/disable the turbo
+	"autopilot", //enable/disable the autopilot
+
+	//network related events
+	"connect", //emitted when a new main client connects
+	"disconnect", //emitted when the main client disconnects
+	"ping", //sent by a client to ping the sever
+	"pong", //response to ping
+
+	//misc events
+	"init", //used to tell a client if he is in control
+	"photo" //used to send a photo to clients (periodically?)
+];
+
+for(var i = 0; i < events.length; i++)
+{
+	var opt = document.createElement("option");
+	opt.value = i;
+	opt.innerHTML = events[i];
+	eventId.appendChild(opt);
+}
+
+function sendEvent()
+{
+	var id = eventId.selectedIndex;
+	var args = eventArgs.value.split(" ");
+	var littleEndian = eventLittleEndian.checked;
+
+	var buff = new ArrayBuffer(16 + args.length);
+	var data = new DataView(buff);
+
+	var now = Date.now();
+	if(littleEndian)
+	{
+		data.setUint32(0, now & 0xFFFFFFFF, true);
+		data.setUint32(4, now >> 32, true);
+	}
+	else
+	{
+		data.setUint32(0, now >> 32, false);
+		data.setUint32(4, now & 0xFFFFFFFF, false);
+	}
+
+	data.setUint32(8, id, littleEndian);
+	data.setUint32(12, args.length, littleEndian);
+
+	for(var i = 0; i < args.length; i++)
+	{
+		data.setUint8(16 + i, parseInt(args[i]));
+	}
+
+	var dump = "";
+	for(var i = 0; i < data.byteLength; i++)
+	{
+		dump += " " + ("00" + data.getUint8(i).toString(16)).slice(-2);
+	}
+	console.log(id, args, dump.trim());
+}
