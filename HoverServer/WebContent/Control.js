caa9528b262310253872a0ce5ea91df8a74db8b3
Jakob Löw <jakob@m4gnus.de>
2018-04-27 14:47:01 +0200

    add an 'intuitive' joystick for controlling the hovercraft

--- HoverServer/WebContent/Control.js
+++ HoverServer/WebContent/Control.js
@@ -1,11 +1,58 @@
-var eventId = document.getElementById("eventId");
-var eventArgs = document.getElementById("eventArgs");
-var eventLittleEndian = document.getElementById("littleEndian");
 var wsUrl = document.getElementById("wsUrl");
-
 var currentWsUrl = "";
 var ws;
+function sendEvent(id, args)
+{
+	if(wsUrl.value != currentWsUrl)
+	{
+		ws = new WebSocket(wsUrl.value);
+		currentWsUrl = wsUrl.value;
 
+		ws.onopen = function()
+		{
+			sendEvent(id, args);
+		};
+		return;
+	}
+
+	var littleEndian = true; //XXX: this is raspberry pi LE mode specific
+
+	var buff = new ArrayBuffer(16 + args.length * 4);
+	var data = new DataView(buff);
+
+	var now = Date.now();
+	if(littleEndian) //TODO this is broken, pls fix
+	{
+		data.setUint32(0, now & 0xFFFFFFFF, true);
+		data.setUint32(4, now >> 32, true);
+	}
+	else
+	{
+		data.setUint32(0, now >> 32, false);
+		data.setUint32(4, now & 0xFFFFFFFF, false);
+	}
+
+	data.setUint32(8, id, littleEndian);
+	data.setUint32(12, args.length * 4, littleEndian);
+
+	for(var i = 0; i < args.length; i++)
+	{
+		data.setInt32(16 + i * 4, args[i], littleEndian);
+	}
+
+	var dump = "";
+	for(var i = 0; i < data.byteLength; i++)
+	{
+		dump += " " + ("00" + data.getUint8(i).toString(16)).slice(-2);
+	}
+	console.log(id, events[id], args, dump.trim());
+
+	if(ws.readyState == ws.OPEN)
+		ws.send(buff);
+}
+
+var eventId = document.getElementById("eventId");
+var eventArgs = document.getElementById("eventArgs");
 var events = [
 	//control related events
 	"hover", //sets the speed of the hover motor
@@ -25,7 +72,6 @@ var events = [
 	"init", //used to tell a client if he is in control
 	"photo" //used to send a photo to clients (periodically?)
 ];
-
 for(var i = 0; i < events.length; i++)
 {
 	var opt = document.createElement("option");
@@ -34,66 +80,90 @@ for(var i = 0; i < events.length; i++)
 	eventId.appendChild(opt);
 }
 
-/*setInterval(function()
+function sendEventFromForm()
 {
-	var val = parseInt(eventArgs.value);
-	if(val >= 500)
-		val = -400;
-	else if(val < 500)
-		val = 1400;
-
-	eventArgs.value = val;
-	sendEvent();
-}, 600);*/
-
-function sendEvent()
-{
-	if(wsUrl.value != currentWsUrl)
-	{
-		ws = new WebSocket(wsUrl.value);
-		currentWsUrl = wsUrl.value;
-
-		ws.onopen = sendEvent;
-		return;
-	}
-
 	var id = eventId.selectedIndex;
 	var args = eventArgs.value.split(" ");
-	var littleEndian = eventLittleEndian.checked;
 
 	if(args.length == 1 && args[0].trim() == "")
 		args = [];
 
-	var buff = new ArrayBuffer(16 + args.length * 4);
-	var data = new DataView(buff);
-
-	var now = Date.now();
-	if(littleEndian)
-	{
-		data.setUint32(0, now & 0xFFFFFFFF, true);
-		data.setUint32(4, now >> 32, true);
-	}
-	else
-	{
-		data.setUint32(0, now >> 32, false);
-		data.setUint32(4, now & 0xFFFFFFFF, false);
-	}
-
-	data.setUint32(8, id, littleEndian);
-	data.setUint32(12, args.length * 4, littleEndian);
-
 	for(var i = 0; i < args.length; i++)
-	{
-		data.setInt32(16 + i * 4, parseInt(args[i]), littleEndian);
-		//data.setUint8(16 + i, parseInt(args[i]));
-	}
+		args[i] = parseInt(args[i]);
 
-	var dump = "";
-	for(var i = 0; i < data.byteLength; i++)
-	{
-		dump += " " + ("00" + data.getUint8(i).toString(16)).slice(-2);
-	}
-	console.log(id, args, dump.trim());
-
-	ws.send(buff);
+	sendEvent(id, args);
 }
+
+var joystick = document.getElementById("joystick");
+//this value defines how many pixels refer to 100% power of a motor
+//bigger values => need to move the joystick further for max power
+var jostickSensitivity = document.getElementById("joystickSensitivity");
+var draw = joystick.getContext('2d');
+var joystickActive = false;
+var originX = 0;
+var originY = 0;
+var sensitivity = jostickSensitivity.value;
+draw.fillStyle = "red";
+draw.strokeStyle = "black";
+draw.lineWidth = 5;
+
+function clamp(val, min, max)
+{
+	return Math.max(min, Math.min(val, max));
+}
+function redraw(x, y)
+{
+	draw.clearRect(0, 0, joystick.width, joystick.height);
+
+	if(!ws || ws.readyState != ws.OPEN)
+		draw.strokeStyle = "red";
+	else
+		draw.strokeStyle = "black";
+	draw.strokeRect(0, 0, joystick.width, joystick.height);
+	draw.strokeStyle = "black";
+
+	if(joystickActive)
+	{
+		draw.beginPath();
+		draw.arc(originX, originY, 100, 0, Math.PI, true);
+		draw.lineTo(originX + 100 + draw.lineWidth / 2, originY);
+		draw.stroke();
+
+		draw.beginPath();
+		draw.arc(originX, originY, 10, 0, 2 * Math.PI);
+		draw.fill();
+
+		draw.beginPath();
+		draw.arc(x, y, 10, 0, 2 * Math.PI);
+		draw.fill();
+	}
+}
+redraw();
+
+joystick.addEventListener("mousedown", function(ev)
+{
+	joystickActive = true;
+	sensitivity = jostickSensitivity.value;
+
+	originX = ev.offsetX;
+	originY = ev.offsetY;
+	redraw(ev.offsetX, ev.offsetY);
+});
+joystick.addEventListener("mouseup", function()
+{
+	joystickActive = false;
+	redraw();
+});
+joystick.addEventListener("mousemove", function(ev)
+{
+	if(joystickActive)
+	{
+		redraw(ev.offsetX, ev.offsetY);
+
+		var speed = (originY - ev.offsetY) * 1000 / sensitivity;
+		sendEvent(1, [clamp(speed, 0, 1000)]);
+
+		var steering = (originX - ev.offsetX) * 1000 / sensitivity + 500;
+		sendEvent(2, [clamp(steering, 0, 1000)]);
+	}
+});
